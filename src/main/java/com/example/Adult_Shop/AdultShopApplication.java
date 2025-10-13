46c1cd53f0c1671ece7b82ebd33b9cc753329314
LokiSkandi <loki.skandinavskiy@mail.ru>
2025-10-13 17:42:14 +0300

    Инициализация проекта

---
+++ src/main/java/com/example/Adult_Shop/AdultShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.Adult_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdultShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdultShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Adult_Shop/AdultShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Adult_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdultShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
